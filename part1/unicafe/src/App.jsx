c1776ad49eb10ceda2f9bc8c18e65efbf3596df8
fvoltess <fedevoltes@gmail.com>
2025-07-03 15:50:51 -0300

    unicafe done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,16 +19,17 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <table>
       <tbody>
-        <StatisticLine text="good"      value={good} />
-        <StatisticLine text="neutral"   value={neutral} />
-        <StatisticLine text="bad"       value={bad} />
-        <StatisticLine text="all"       value={all} />
-        <StatisticLine text="average"   value={average} />
-        <StatisticLine text="positive"  value={positive} />
+        <StatisticLine text="good"     value={good} />
+        <StatisticLine text="neutral"  value={neutral} />
+        <StatisticLine text="bad"      value={bad} />
+        <StatisticLine text="all"      value={all} />
+        <StatisticLine text="average"  value={average} />
+        <StatisticLine text="positive" value={positive} />
       </tbody>
     </table>
   )
 }
+
 
 const App = () => {
   const [good,    setGood]    = useState(0)
